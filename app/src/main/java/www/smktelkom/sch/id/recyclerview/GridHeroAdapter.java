32c67aca434cabbf947f9a54f6c97be864182897
Aaron <60208227+dewangga18@users.noreply.github.com>
2020-09-27 12:18:37 +0700

    Update grid

--- app/src/main/java/www/smktelkom/sch/id/recyclerview/GridHeroAdapter.java
+++ app/src/main/java/www/smktelkom/sch/id/recyclerview/GridHeroAdapter.java
@@ -18,18 +18,28 @@ public class GridHeroAdapter extends RecyclerView.Adapter<GridHeroAdapter.GridVi
     public GridHeroAdapter(ArrayList<Hero> list){
         this.listHero = list;
     }
+    private OnItemClickCallback onItemClickCallback;
+    public void setOnItemClickCallback(OnItemClickCallback onItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback;
+    }
 
     @NonNull
     @Override
     public GridViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_grid_hero, viewGroup, false);
-            return new GridViewHolder(view);
+        return new GridViewHolder(view);
 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull GridViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final GridViewHolder holder, int position) {
         Glide.with(holder.itemView.getContext()).load(listHero.get(position).getPhoto()).apply(new RequestOptions().override(350,550)).into(holder.imgPhoto);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClickCallback.onItemClicked(listHero.get(holder.getAdapterPosition()));
+            }
+        });
     }
 
     @Override
@@ -43,5 +53,8 @@ public class GridHeroAdapter extends RecyclerView.Adapter<GridHeroAdapter.GridVi
             super(itemView);
             imgPhoto = itemView.findViewById(R.id.img_item_photo);
         }
+    }
+    public interface OnItemClickCallback {
+        void onItemClicked(Hero data);
     }
 }
